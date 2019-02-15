5a6839030195806b798fab427d7df76bc98b6cfe
yinjimmy <jimmy.yin5@gmail.com>
2019-02-15 17:09:53 +0800

    update libcurl & openssl

--- cocos2dx/platform/third_party/android/prebuilt/libcurl/Android.mk
+++ cocos2dx/platform/third_party/android/prebuilt/libcurl/Android.mk
@@ -5,11 +5,13 @@ LOCAL_MODULE := cocos_curl_static
 LOCAL_MODULE_FILENAME := curl
 LOCAL_SRC_FILES := libs/$(TARGET_ARCH_ABI)/libcurl.a
 
-ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
-	LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include64
-else
-	LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
-endif
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+
+# ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
+# 	LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include64
+# else
+# 	LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+# endif
 
 LOCAL_STATIC_LIBRARIES += cocos_ssl_static
 LOCAL_STATIC_LIBRARIES += cocos_crypto_static
